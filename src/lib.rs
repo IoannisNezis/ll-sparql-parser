c2033ef107e9c2fc8368abf99dd54a85b836a170
Ioannis Nezis <ioannis@nezis.de>
2025-02-26 16:09:38 +0100

    FEAT: mark SyntaxElements as active based of cursor position

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,19 @@ mod syntax_kind;
 mod syntax_node;
 
 use js_sys::{Array, Object, Reflect};
+use rowan::TextSize;
 use syntax_node::SyntaxNode;
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 use syntax_kind::SyntaxKind;
 
 #[wasm_bindgen]
-pub fn get_parse_tree(input: &str) -> JsValue {
+pub fn get_parse_tree(input: &str, offset: u32) -> JsValue {
     let root = SyntaxNode::new_root(parser::parse_text(input));
-    build_js_tree(&root)
+    build_js_tree(&root, TextSize::new(offset))
 }
 
-fn build_js_tree(node: &SyntaxNode) -> JsValue {
+fn build_js_tree(node: &SyntaxNode, offset: TextSize) -> JsValue {
     let obj = Object::new();
     Reflect::set(
         &obj,
@@ -23,8 +24,15 @@ fn build_js_tree(node: &SyntaxNode) -> JsValue {
     )
     .unwrap();
     Reflect::set(&obj, &JsValue::from_str("type"), &JsValue::from_str("node")).unwrap();
+    Reflect::set(
+        &obj,
+        &JsValue::from_str("active"),
+        &JsValue::from_bool(node.text_range().contains(offset)),
+    )
+    .unwrap();
+
     let children = Array::from_iter(node.children_with_tokens().filter_map(|child| match child {
-        rowan::NodeOrToken::Node(node) => Some(build_js_tree(&node)),
+        rowan::NodeOrToken::Node(node) => Some(build_js_tree(&node, offset)),
         rowan::NodeOrToken::Token(token) if token.kind() != SyntaxKind::WHITESPACE => {
             let token_obj = Object::new();
             Reflect::set(
@@ -43,6 +51,12 @@ fn build_js_tree(node: &SyntaxNode) -> JsValue {
                 &token_obj,
                 &JsValue::from_str("text"),
                 &JsValue::from_str(token.text()),
+            )
+            .unwrap();
+            Reflect::set(
+                &token_obj,
+                &JsValue::from_str("active"),
+                &JsValue::from_bool(token.text_range().contains(offset)),
             )
             .unwrap();
             Some(token_obj.into())
